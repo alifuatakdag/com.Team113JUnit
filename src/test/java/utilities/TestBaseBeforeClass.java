9e10a7aaf8d0fc67065fcac28ff69a0057d41d3a
alifuatakdag <akdag.alifuat@gmail.com>
2023-05-11 13:19:32 +0300

    8.gun

--- src/test/java/utilities/TestBaseBeforeClass.java
+++ src/test/java/utilities/TestBaseBeforeClass.java
@@ -1,24 +1,19 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class TestBase {
-    /*
-        TestBase class'ini
-        bu class'i parent edinecek class'larda
-        driver olusturmamak ve ayarlari yeniden yapmamak icin kullaniyoruz
-     */
+public class TestBaseBeforeClass {
 
-    protected WebDriver driver;
+    protected static WebDriver driver;
 
-    @Before
-    public void setup(){
+    @BeforeClass
+    public static void setup(){
         WebDriverManager.chromedriver().setup();
         driver= new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,8 +22,8 @@ public class TestBase {
     }
 
 
-    @After
-    public void teardown(){
+    @AfterClass
+    public static void teardown(){
         driver.close();
     }
 }
